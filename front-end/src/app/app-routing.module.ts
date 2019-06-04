06e7fa67d7fd70cb3de50b3277b616b162a62b11
musthafamadhahi <musthafamadhahi@gmail.com>
2019-06-04 06:29:29 +0530

    SignIn, SignUp

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './components/auth/user/user.component';
+import { SignInComponent } from './components/auth/user/sing-in/sing-in.component';
+import { SignUpComponent } from './components/auth/user/sing-up/sing-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup', component: UserComponent,
+    children: [{ path: '', component: SignUpComponent }]
+},
+{
+    path: 'login', component: UserComponent,
+    children: [{ path: '', component: SignInComponent }]
+},
+{
+    path: '', redirectTo: '/login', pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './components/auth/user/user.component';
+import { SingInComponent } from './components/auth/user/sing-in/sing-in.component';
+import { SingUpComponent } from './components/auth/user/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SingInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
